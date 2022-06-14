16221555fdfaa445cd683abf3c61f16a1c1df58b
Kopal Garg <Kopal.Garg@ibm.com>
2022-06-14 08:14:47 -0400

    starter files

---
+++ starter_files/visual-similarity.py
@@ -0,0 +1,11 @@
+# Import required libraries 
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.metrics import pairwise_distances
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+import requests
+import urllib.request
+
+
