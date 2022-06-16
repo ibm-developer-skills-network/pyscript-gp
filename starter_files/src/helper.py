1acda963f973e023d508e786928e7c7661986dd8
Kopal Garg <Kopal.Garg@ibm.com>
2022-06-16 02:59:01 -0400

    update starter code

---
+++ starter_files/src/helper.py
@@ -0,0 +1,12 @@
+# Import required libraries 
+import numpy as np 
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder 
+from sklearn.preprocessing import MinMaxScaler 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor 
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_squared_error 
+from sklearn.metrics import r2_score
+
+
